2d666bd1d5d0d78c1b3346c07d2823945d8d0ab1
mwun <michael.wunderlich@meteor-ent.com>
2014-03-24 19:59:12 -0700

    ch11

---
+++ Ch11CardLib/Ch11CardLib/Card.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch10CardLib
+{
+	public class Card
+	{
+		public readonly Suit _suit;
+		public readonly Rank _rank;
+
+		public Card(Suit newSuit, Rank newRank)
+		{
+			_suit = newSuit;
+			_rank = newRank;
+		}
+
+		private Card()
+		{
+		}
+
+		public override string ToString()
+		{
+			return "The " + _rank + " of " + _suit + "s";
+		}
+	}
+}
